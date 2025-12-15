6c4a9e5455e94b1888efc6619d067d1fc6524a4c
Sebi Barta <sebibarta11@gmail.com>
2025-12-15 14:34:00 +0200

    Add base frontend. Add container support, start with backend

--- ITrade.AppHost/AppHost.cs
+++ ITrade.AppHost/AppHost.cs
@@ -5,8 +5,13 @@ var db = builder.AddPostgres("db")
     .WithPgWeb()
     .AddDatabase("ITradeDB");
 
-builder.AddProject<Projects.ITrade_ApiServices>("itrade-apiservices")
+var apiService = builder.AddProject<Projects.ITrade_ApiServices>("itrade-apiservices")
     .WithReference(db)
     .WaitFor(db);
+
+var frontend = builder.AddContainer("itrade-frontend", "itrade-frontend")
+    .WithDockerfile("../ITrade.UserClient/itrade")
+    .WithHttpEndpoint(port: 80, targetPort: 80, name: "http")
+    .WithEnvironment("VITE_API_URL", apiService.GetEndpoint("http"));
 
 builder.Build().Run();
